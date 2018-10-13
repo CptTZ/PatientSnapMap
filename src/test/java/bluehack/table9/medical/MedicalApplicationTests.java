29f453b9146a8b8906f4290601111556b0acc8bf
TonyZ <zyc@gis4.org>
2018-10-13 12:51:20 -0400

    init

---
+++ src/test/java/bluehack/table9/medical/MedicalApplicationTests.java
@@ -0,0 +1,16 @@
+package bluehack.table9.medical;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
