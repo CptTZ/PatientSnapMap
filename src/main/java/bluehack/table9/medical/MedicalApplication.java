653953c3e2152dfd448c9c30f7b3e7f49f36643e
TonyZ <zyc@gis4.org>
2018-10-13 13:07:52 -0400

    basic setup

--- src/main/java/bluehack/table9/medical/MedicalApplication.java
+++ src/main/java/bluehack/table9/medical/MedicalApplication.java
@@ -1,12 +1,31 @@
 package bluehack.table9.medical;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MedicalApplication {
+import java.util.TimeZone;
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalApplication.class, args);
-	}
+@SpringBootApplication
+public class MedicalApplication implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(MedicalApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+        if (args.getSourceArgs().length > 0)
+            logger.warn("Command-line arguments: {}", String.join(",", args.getSourceArgs()));
+        for (String name : args.getOptionNames()) {
+            logger.warn(String.format("Non-option->%s=%s", name, args.getOptionValues(name)));
+        }
+    }
+
 }
