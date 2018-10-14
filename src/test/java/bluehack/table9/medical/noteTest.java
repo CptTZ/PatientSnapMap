49ba43f336f179fca57b243ed23102746bd69fb0
TonyZ <i@cpttony.me>
2018-10-14 01:55:00 -0400

    Save notes with positions into MongoDB.

--- src/test/java/bluehack/table9/medical/noteTest.java
+++ src/test/java/bluehack/table9/medical/noteTest.java
@@ -22,16 +22,25 @@ public class noteTest {
 
     @Test
     public void parseNoteTest() throws Exception {
-        List<String> conData = Files.readAllLines(Paths.get("seizure1.con"));
-        List<String> originalData = Files.readAllLines(Paths.get("seizure1.txt"));
+        for (int i = 1; i <= 8; i++) {
+            parseNode(i);
+        }
+    }
 
-        NoteEntity n = extractInfo(conData);
+    private void parseNode(int i) throws Exception {
+        String f1 = String.format("text/seizure%d.con", i);
+        String f2 = String.format("text/seizure%d.txt", i);
+
+        List<String> conData = Files.readAllLines(Paths.get(f1));
+        List<String> originalData = Files.readAllLines(Paths.get(f2));
+
+        NoteEntity n = extractNlpResult(originalData, conData);
         n.setNote(String.join("\r\n", originalData));
 
         this.noteDao.saveNote(n);
     }
 
-    private NoteEntity extractInfo(List<String> conData) {
+    private NoteEntity extractNlpResult(List<String> originalData, List<String> conData) {
         NoteEntity n = new NoteEntity();
 
         ArrayList<String[]> problem = new ArrayList<>();
@@ -39,17 +48,26 @@ public class noteTest {
         ArrayList<String[]> test = new ArrayList<>();
 
         for (String line : conData) {
-            String[] d = line.split(",");
-            String[] dd = new String[]{d[1], d[2]};
+            String[] d = line.split("\t");
+            // NLP starts with 1
+            int whichLine = Integer.parseInt(d[1]) - 1,
+                    startPos = Integer.parseInt(d[2]),
+                    endPos = Integer.parseInt(d[3]);
+            String tipText = d[4].trim();
+
+            System.err.print(whichLine + 1 + ": ");
+            int[] sePos = calcStartEndPosition(originalData.get(whichLine), startPos, endPos, tipText.toLowerCase());
+
+            String[] tmp = new String[]{String.valueOf(whichLine), String.valueOf(sePos[0]), String.valueOf(sePos[1]), tipText};
             switch (d[0]) {
                 case "problem":
-                    problem.add(dd);
+                    problem.add(tmp);
                     break;
                 case "test":
-                    test.add(dd);
+                    test.add(tmp);
                     break;
                 case "treatment":
-                    treat.add(dd);
+                    treat.add(tmp);
                     break;
             }
         }
@@ -58,6 +76,16 @@ public class noteTest {
         n.setTreatment(treat);
         n.setProblem(problem);
         return n;
+    }
+
+    private int[] calcStartEndPosition(String origLine, int startTokenPos, int endTokenPos, String tip) {
+        tip = tip.replace("   ", "\t");
+        tip = tip.replace("__num__", "");
+        int left = origLine.toLowerCase().indexOf(tip);
+        int right = left + tip.length();
+
+        System.err.println(String.format("%d\t%d\t%s%n", left + 1, right + 1, origLine.substring(left, right)));
+        return new int[]{left, right};
     }
 
 }
